b485b5d252e55b8d6efbcd6cefcca32349aad759
NqobileBuhle <139454693+NqobileBuhle@users.noreply.github.com>
2024-09-02 13:30:34 +0200

    Done with About section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import About from "./components/About/About";
 import Hero from "./components/Hero/Hero";
 export default function App() {
   return (
     <section>
       <Hero />
+      <About/>
     </section>
   );
 }
