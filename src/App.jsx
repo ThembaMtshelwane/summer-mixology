01c0fbd38857dc2ca94b7183f171b5fbebbabdf0
codycubes <boitshepomashamaite777@gmail.com>
2024-09-02 12:26:36 +0200

    product layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Hero from "./components/Hero/Hero";
+import ProductSection from "./components/ProductSection/ProductSection";
 export default function App() {
   return (
     <section>
       <Hero />
+      <ProductSection />
     </section>
   );
 }
