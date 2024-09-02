c4e3c494bd552e13c2334cfc4ee36408b25904ba
ThembaMtshelwane <thembamm3@gmail.com>
2024-09-02 14:10:54 +0200

    Hero: draft 1

---
+++ src/components/EmblaCarousel.jsx
@@ -0,0 +1,62 @@
+import useEmblaCarousel from "embla-carousel-react";
+import { heroImgaes } from "../localData";
+import Autoplay from "embla-carousel-autoplay";
+
+export function EmblaCarousel() {
+  //   const [emblaRef] = useEmblaCarousel({ loop: true }, [Autoplay()]);
+  const [emblaRef] = useEmblaCarousel();
+
+  return (
+    <div className="embla border-2 border-red-600 h-full w-full" ref={emblaRef}>
+      <div className="embla__container border-2 border-blue-300 h-full w-full">
+        <div className="embla__slide border-2 border-yellow-400 h-full w-full relative flex items-center justify-center md:justify-end">
+          <section className="border-2 h-full  w-full relative">
+            <img
+              className="w-full h-full object-cover "
+              src="hero/1.jpg"
+              alt=""
+            />
+          </section>
+          <section className="absolute w-full border-2 border-red-500 p-2 flex flex-col items-center justify-center sm:w-[500px]  md:mr-[15%]">
+            <h1 className=" text-6xl uppercase text-center sm:text-7xl md:text-right md:text-8xl">
+              the art of the cocktail
+            </h1>
+            <button className="md:ml-auto">View Collection</button>
+          </section>
+        </div>
+
+        <div className="embla__slide border-2 border-yellow-400 h-full w-full relative flex items-center justify-center md:justify-end">
+          <section className="border-[10px] h-full w-full border-purple-700">
+            <img
+              className="w-full h-full object-cover "
+              src="hero/2.jpg"
+              alt=""
+            />
+          </section>
+          <section className=" absolute w-full border-2 h-fit border-red-500 p-2 flex flex-col items-center justify-center sm:w-[600px] md:[600px]  md:left-32">
+            <h1 className=" text-6xl uppercase text-center sm:text-7xl md:text-left md:text-8xl">
+              create professional cocktails
+            </h1>
+            <button className="md:mr-auto bg-white">View Collection</button>
+          </section>
+        </div>
+
+        <div className="embla__slide border-2 border-yellow-400 h-full w-full relative flex items-center justify-center md:justify-end">
+          <section className="border-[10px] h-full w-full border-purple-700">
+            <img
+              className="w-full h-full object-cover "
+              src="hero/3.jpg"
+              alt=""
+            />
+          </section>
+          <section className=" absolute w-full border-2 h-fit border-red-500 p-2 flex flex-col items-center justify-center sm:w-[600px] md:[600px]  md:left-32">
+            <h1 className=" text-6xl uppercase text-center sm:text-7xl md:text-left md:text-8xl">
+              from zero to the new bar hero
+            </h1>
+            <button className="md:mr-auto bg-white">View Collection</button>
+          </section>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/localData.js
@@ -0,0 +1,8 @@
+export const heroImgaes = [
+  "hero/1.jpg",
+  "hero/2.jpg",
+  "hero/3.jpg",
+  "hero/4.jpg",
+  "hero/5.jpg",
+  "hero/6.jpg",
+];
