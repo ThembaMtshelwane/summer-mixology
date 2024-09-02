28db577243dd34cac9053ca88da3a0540558bcf7
codycubes <boitshepomashamaite777@gmail.com>
2024-09-02 14:45:20 +0200

    productSection part1

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Hero from "./components/Hero/Hero";
 import ProductSection from "./components/ProductSection/ProductSection";
 export default function App() {
   return (
-    <section>
+    <section className="mx-[30px]">
       <Hero />
       <ProductSection />
     </section>
