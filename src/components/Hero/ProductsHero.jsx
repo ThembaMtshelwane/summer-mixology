a67cd2666f0eb714e621ce7b85104ed60acf1ded
ThembaMtshelwane <thembamm3@gmail.com>
2024-09-04 12:06:36 +0200

    Product details: Hero section

--- src/components/Hero/ProductsHero.jsx
+++ src/components/Hero/ProductsHero.jsx
@@ -1,9 +1,24 @@
 import { useMatch } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { useDrinks } from "../context/DrinksContext";
 
 const ProductsHero = () => {
   // const { name } = useParams();
   const match = useMatch("/products/");
   const match2 = useMatch("/categories/");
+  const match3 = useMatch("/products/:name");
+
+  const { name } = useParams();
+  const { drinks } = useDrinks();
+  let category = null;
+
+  if (!drinks) return <div>Loading...</div>;
+
+  if (name) {
+    const drink = drinks.find((drink) => drink.name === name);
+    category = drink.category;
+  }
+
   return (
     <section
       className="bg-blue-900 h-[70vh] flex flex-col justify-center items-center"
@@ -24,6 +39,14 @@ const ProductsHero = () => {
         {match2 ? (
           <h1 className="uppercase text-5xl text-[#173e59] font-semibold sm:text-7xl">
             The Categories
+          </h1>
+        ) : (
+          ""
+        )}
+
+        {match3 ? (
+          <h1 className="uppercase text-5xl text-[#173e59] font-semibold sm:text-7xl">
+            {category && category}
           </h1>
         ) : (
           ""
--- src/components/ProductSection/ProductDetails.jsx
+++ src/components/ProductSection/ProductDetails.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import { useDrinks } from '../context/DrinksContext';
-import ProductSection from './ProductSection';
+// import React from "react";
+import { useParams } from "react-router-dom";
+import { useDrinks } from "../context/DrinksContext";
+import ProductSection from "./ProductSection";
+// import Navbar from "../Hero/Navbar/Navbar";
 
 const ProductDetails = () => {
-  const { name } = useParams(); 
+  const { name } = useParams();
   const { drinks } = useDrinks();
 
   if (!drinks) return <div>Loading...</div>;
@@ -17,23 +18,22 @@ const ProductDetails = () => {
 
   return (
     <>
-      <div className="relative w-full h-[60vh] bg-white overflow-hidden">
-        <div className="absolute inset-0 flex flex-col justify-center items-end text-[#002B49] z-10">
-          <h2 className="text-xl font-normal text-center w-full">
-            {category}
-          </h2>
-          <h1 className="text-9xl font-normal text-center w-full text-gray-600">
-            {name}
-          </h1>
-        </div>
+      <div className="absolute w-full h-[60vh] bg-white overflow-hidden">
+        <h2 className="text-xl font-normal text-center w-full">{category}</h2>
+        <h1 className="text-9xl font-normal text-center w-full text-gray-600">
+          {name}
+        </h1>
       </div>
 
       <div className="w-full">
-        <img src={thumbnail} alt={thumbnail} className="w-full h-[70vh] object-cover"/>
+        <img
+          src={thumbnail}
+          alt={thumbnail}
+          className="w-full h-[70vh] object-cover"
+        />
       </div>
 
       <div className="relative flex flex-col md:flex-row md:items-start w-full">
-
         {/* Ingredients Section */}
         <div className="md:relative top-[-20px] md:top-[-60px] left-0 md:w-[40vw] w-full h-[60vh] p-14 bg-[#002b49]">
           <h1 className="text-5xl text-white font-bold mb-4">INGREDIENTS</h1>
@@ -48,17 +48,16 @@ const ProductDetails = () => {
 
         {/* Preparation Section */}
         <div className="relative md:w-[60vw] w-full md:pt-[8rem] pt-[8rem] md:pb-16 pb-8 p-8 bg-white text-[#002b49]">
-          <h1 className="text-5xl font-bold mb-4 text-[#002b49]">PREPARATION</h1>
+          <h1 className="text-5xl font-bold mb-4 text-[#002b49]">
+            PREPARATION
+          </h1>
           <p className="text-lg">{instructions}</p>
         </div>
       </div>
 
-
-
-      <div className='mx-20'>
+      <div className="mx-20">
         <ProductSection />
       </div>
-      
     </>
   );
 };
